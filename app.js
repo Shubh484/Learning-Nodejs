cfa36c4b23e1b111518a2f0922b0a1da1d519171
Shubh <shubhsingh8767@gmail.com>
2023-12-16 15:41:33 +0530

    done with http basics

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.write("welcome to our home page");
-  res.end();
+  if (req.url === "/") {
+    // '/' denotes home page
+    res.end("welcome to our home page");
+  }
+  if (req.url === "/about") {
+    res.end("Here is our short history");
+  }
+  res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">get back home</a>`);
 });
 
 server.listen(4000); // go chech at localhost:4000 on web browser for response
