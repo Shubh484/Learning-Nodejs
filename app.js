0baf8df8c9de7c7d91d43494099c5f823578adb8
Shubh <shubhsingh8767@gmail.com>
2023-12-16 13:41:48 +0530

    fs module(sync) x 2

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const { readFileSync, writeFileSync } = require("fs");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
-console.log(first, second);
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result: ${first},${second}`,
+  { flag: "a" } // {flag:"a" -> is used to append}
+);
