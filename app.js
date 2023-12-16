7aaf2c97f20dd81324d946146d627d14c1f5a86f
Shubh <shubhsingh8767@gmail.com>
2023-12-16 13:17:05 +0530

    fs module(sync)

---
+++ app.js
@@ -0,0 +1,6 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+console.log(first, second);
